356b6d39690312788844460f2dfe0ee22e72fdad
Pierre Prinetti <me@qrawl.net>
2016-07-01 12:08:20 +0200

    typos

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ if [ -z ${email+x} ]; then echo "Fatal: administrator email address must be spec
 if [ -z ${domains+x} ]; then echo "Fatal: domains must be specified with the environment variable named 'domains'"; exit 1; fi
 #if [ -z ${agree_tos+x} ]; then echo "Fatal: agree to the TOS setting the environment variable named 'agree_tos'"; exit 1; fi
 
-letsencrypt certonly -- --verbose --noninteractive --standalone --agree-tos --email <${email}> -d "${domains}"
+letsencrypt certonly --verbose --noninteractive --standalone --agree-tos --email=<${email}> -d "${domains}"
