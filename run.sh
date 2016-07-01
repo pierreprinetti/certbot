eedbc95d2838039f1d1425010fb510924294dfb9
Pierre Prinetti <me@qrawl.net>
2016-07-01 11:55:45 +0200

    TOS and email

--- run.sh
+++ run.sh
@@ -2,4 +2,8 @@
 
 echo "Running Certbot for domains: ${domains}"
 
-letsencrypt certonly --verbose --noninteractive -d "${domains}"
+if [ -z ${email+x} ]; then echo "Fatal: administrator email address must be specified with the environment variable named 'email'"; exit 1; fi
+if [ -z ${domains+x} ]; then echo "Fatal: domains must be specified with the environment variable named 'domains'"; exit 1; fi
+#if [ -z ${agree_tos+x} ]; then echo "Fatal: agree to the TOS setting the environment variable named 'agree_tos'"; exit 1; fi
+
+letsencrypt certonly -- --verbose --noninteractive --standalone --agree-tos --email <${email}> -d "${domains}"
